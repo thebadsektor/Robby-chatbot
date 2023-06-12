4c79f8cd23d75758aa5cfcebe706aa5ba70c8d80
thebadsektor <76891921+thebadsektor@users.noreply.github.com>
2023-06-12 13:35:35 +0800

    Update tuto_chatbot_csv.py

--- tuto_chatbot_csv.py
+++ tuto_chatbot_csv.py
@@ -8,7 +8,10 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 import tempfile
+import os
 
+# api_key = os.environ.get("OPENAI_API_KEY")
+api_key = os.environ.get(st.secrets["OPENAI_API_KEY"])
 
 user_api_key = st.sidebar.text_input(
     label="#### Your OpenAI API key 👇",
@@ -28,7 +31,7 @@ if uploaded_file :
     embeddings = OpenAIEmbeddings()
     vectors = FAISS.from_documents(data, embeddings)
 
-    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=user_api_key),
+    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=api_key),
                                                                       retriever=vectors.as_retriever())
 
     def conversational_chat(query):
